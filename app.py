4db414f6e4dff783db73c40e17b8ef53d70d4736
yurley sanchez <yurley.sanchez@yurleys-MacBook-Pro.local>
2020-12-05 09:47:14 -0500

    remove todo

--- app.py
+++ app.py
@@ -5,13 +5,8 @@ import random
 app = Flask(__name__)
 
 
-@app.route('/juego/<int:numero>')
-def play(numero):
-    ran = random.randint(1, 10)
-    print('ran', ran)
-    if ran == numero: 
-        return 'Adivinaste'
-    else: 
-        return 'sigue intentando'
+@app.route('/')
+def play():
+    return 'hola mundo'
 
 
