2cdebd23af697b5bcd16d37ad18e915fb6753b37
yurley sanchez <yurley.sanchez@yurleys-MacBook-Pro.local>
2020-12-05 11:05:25 -0500

    save calculadora

--- app.py
+++ app.py
@@ -6,14 +6,60 @@ import random
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
-def play():
+@app.route('/suma', methods=['POST', 'GET'])
+def suma():
     respuesta = 'no tengo respuesta'
     if request.method == 'POST':
         valor_1 = int(request.form['valor_1'])
         valor_2 = int(request.form['valor_2'])
 
         respuesta = valor_1 + valor_2
+
+    return render_template(
+        'index.html',
+        respuesta=respuesta
+    )
+
+
+@app.route('/resta', methods=['POST', 'GET'])
+def resta():
+    respuesta = 'no tengo respuesta'
+    if request.method == 'POST':
+        valor_1 = int(request.form['valor_1'])
+        valor_2 = int(request.form['valor_2'])
+
+        respuesta = valor_1 - valor_2
+
+    return render_template(
+        'index.html',
+        respuesta=respuesta
+    )
+
+
+
+@app.route('/multiplicacion', methods=['POST', 'GET'])
+def multiplicacion():
+    respuesta = 'no tengo respuesta'
+    if request.method == 'POST':
+        valor_1 = int(request.form['valor_1'])
+        valor_2 = int(request.form['valor_2'])
+
+        respuesta = valor_1 * valor_2
+
+    return render_template(
+        'index.html',
+        respuesta=respuesta
+    )
+
+
+@app.route('/division', methods=['POST', 'GET'])
+def division():
+    respuesta = 'no tengo respuesta'
+    if request.method == 'POST':
+        valor_1 = int(request.form['valor_1'])
+        valor_2 = int(request.form['valor_2'])
+
+        respuesta = valor_1 / valor_2
 
     return render_template(
         'index.html',
