1009c5f237b5eb8c6afea565c06e1b56ec5d8536
yurley sanchez <yurley.sanchez@yurleys-MacBook-Pro.local>
2020-12-05 10:40:47 -0500

    save multiple inputs

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from markupsafe import escape
-from flask import render_template
 
 import random
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def play():
-    lenguaje = 'C++'
-    persona = 'Katterine'
+    respuesta = 'no tengo respuesta'
+    if request.method == 'POST':
+        valor_1 = int(request.form['valor_1'])
+        valor_2 = int(request.form['valor_2'])
+
+        respuesta = valor_1 + valor_2
+
     return render_template(
-        'index.html', 
-        lenguaje=lenguaje,
-        persona=persona
+        'index.html',
+        respuesta=respuesta
     )
 
 
